a4831ad36542d20bb6976831073bc398e08ee8e7
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-03-31 21:35:10 +0530

    code to find  elements from the above array is sorted in decreasing Order or not if it is sorted it prints true otherwise it prints false

---
+++ src/ArrayInJava/SortedOrNotInDecreasingOrder.java
@@ -0,0 +1,39 @@
+package ArrayInJava;
+import java.util.Scanner;
+public class SortedOrNotInDecreasingOrder {
+    public static void PrintArray(int []array){
+        for(int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+    public static boolean SortedOrNot(int[]array){
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]>array[j]&& array[j]>array[j+1]){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        int[] array=new int[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<array.length;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Our array is");
+        PrintArray(array);
+        System.out.println("The above array is Sorted in Decreasing Order this is "+ SortedOrNot(array));
+
+
+
+    }
+}

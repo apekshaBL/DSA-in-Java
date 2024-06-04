da02a7468537585e8d707f1d1e93889b66ad093c
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-04 18:55:26 +0530

    Power(input from user) of Digit(input from user) is found by recursion

---
+++ src/Recursion/PowerOfNumber.java
@@ -0,0 +1,21 @@
+package Recursion;
+import java.util.Scanner;
+public class PowerOfNumber {
+    static int Power(int p,int q){
+        if(q==1 || q==0){
+            return p;
+        }
+        int n=Power(p,q-1);
+        return n*p;
+
+
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int p=sc.nextInt();
+        System.out.println("Enter the power");
+        int q=sc.nextInt();
+        System.out.println(Power(p,q));
+    }
+}

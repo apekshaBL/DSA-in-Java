c9d2c6dde57ad5287cc5fd8aa73b02fd4f1f06ab
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-04 22:31:01 +0530

    The alternate sum of number entered by user is found my recursion

---
+++ src/Recursion/Alternate_Sum_Of_N_number.java
@@ -0,0 +1,25 @@
+package Recursion;
+import java.util.Scanner;
+public class Alternate_Sum_Of_N_number {
+    static int Sum(int n){
+        if(n==0){
+            return 0;
+        }
+        if(n%2==0){
+            return Sum(n-1)-n;
+        }
+        else{
+            return Sum(n-1)+n;
+        }
+
+
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        System.out.println(Sum(n));
+
+
+    }
+}

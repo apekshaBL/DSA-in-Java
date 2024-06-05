60ac28e82a9e10a77f60a496eda1aeb2d432e964
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-05 16:25:56 +0530

    Searching the index of target element taken input from user in Array by Recursion

---
+++ src/Recursion/IndexOfSearchElem.java
@@ -0,0 +1,38 @@
+package Recursion;
+import java.util.Scanner;
+public class IndexOfSearchElem {
+    static void PrintArray(int[]array,int idx){
+        if(idx==array.length){
+            return;
+        }
+        System.out.println(array[idx]);
+        PrintArray(array,idx+1);
+    }
+    static int Searched(int[]array,int target,int idx){
+        int i=idx;
+        if(idx>=array.length-1){
+            return -1;
+        }
+        if(array[idx]==target){
+            return idx;
+        }
+        return Searched(array,target,idx+1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n=sc.nextInt();
+        System.out.println("Enter elements of array");
+        int[]array=new int[n];
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+        System.out.println("Our array is");
+        PrintArray(array,0);
+        System.out.println("Enter target");
+        int target=sc.nextInt();
+        System.out.println(Searched(array,target,0));
+
+    }
+}

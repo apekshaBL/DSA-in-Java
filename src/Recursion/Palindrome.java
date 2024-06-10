e025a45ad7ce77d92478f861f2a2c0b27cdf1ae7
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-10 10:42:24 +0530

    Code to find given String is Palindrome or not using Recursion

---
+++ src/Recursion/Palindrome.java
@@ -0,0 +1,24 @@
+package Recursion;
+import  java.util.Scanner;
+public class Palindrome {
+    static String Check(String name,int idx){
+
+        if(idx==name.length()){
+            return "";
+        }
+        String ans=Check(name,idx+1);
+        String reverse=ans+name.charAt(idx);
+        return reverse;
+    }
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the String");
+        String name=sc.nextLine();
+        if(Check(name,0).equals(name)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}

5731d1cc6a1a792a60166552f86d1c376628be6d
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-15 13:38:27 +0530

    Displaying all the elements of linked list when size of linked list is not known by recursion

--- src/LinkedList/DisplayLinkedList.java
+++ src/LinkedList/DisplayLinkedList.java
@@ -1,12 +1,20 @@
 package LinkedList;
 
 public class DisplayLinkedList {
-    public static void Display(Node head){
-        while(head!=null){
-            System.out.print(head.data+" ");
-            head=head.next;
+    public static void RecursiveDisplay(Node Head){
+        if(Head==null){
+            return;
         }
+        System.out.println(Head.data);
+        RecursiveDisplay(Head.next);
+
     }
+//    public static void Display(Node head){
+//        while(head!=null){
+//            System.out.print(head.data+" ");
+//            head=head.next;
+//        }
+//    }
     public static class Node{
         int data;
         Node next;
@@ -23,6 +31,7 @@ public class DisplayLinkedList {
         Node f=new Node(30);
         Node g=new Node(35);
         Node h=new Node(40);
+        Node i=new Node(45);
         //a=>b=>c=>d=>e=.f
         a.next=b;
         b.next=c;
@@ -31,6 +40,7 @@ public class DisplayLinkedList {
         e.next=f;
         f.next=g;
         g.next=h;
+        h.next=i;
         Node temp=a;
 //        for(int i=0;i<6;i++){
 //            System.out.print(temp.data + " ");
@@ -41,7 +51,8 @@ public class DisplayLinkedList {
 //            System.out.print(temp.data+" ");
 //            temp=temp.next;
 //        }
-        Display(temp);
+       // Display(temp);
+        RecursiveDisplay(temp);
 
     }
 }

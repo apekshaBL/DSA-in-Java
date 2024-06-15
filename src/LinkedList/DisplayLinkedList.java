d309e4b757e2e8b3e51475a8cd452e0d65110752
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-15 13:02:48 +0530

    Displaying all the elements of linked list

---
+++ src/LinkedList/DisplayLinkedList.java
@@ -0,0 +1,30 @@
+package LinkedList;
+
+public class DisplayLinkedList {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a=new Node(5);
+        Node b=new Node(10);
+        Node c=new Node(15);
+        Node d=new Node(20);
+        Node e=new Node(25);
+        Node f=new Node(30);
+        //a=>b=>c=>d=>e=.f
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=f;
+        Node temp=a;
+        for(int i=0;i<6;i++){
+            System.out.print(temp.data + " ");
+            temp=temp.next;
+        }
+    }
+}

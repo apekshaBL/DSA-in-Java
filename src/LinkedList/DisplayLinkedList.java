bdfe0eae023a8e9979f0be7e8add980892c43cb5
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-06-15 13:15:37 +0530

    Displaying all the elements of linked list when size of linked list is not known

--- src/LinkedList/DisplayLinkedList.java
+++ src/LinkedList/DisplayLinkedList.java
@@ -15,16 +15,24 @@ public class DisplayLinkedList {
         Node d=new Node(20);
         Node e=new Node(25);
         Node f=new Node(30);
+        Node g=new Node(35);
         //a=>b=>c=>d=>e=.f
         a.next=b;
         b.next=c;
         c.next=d;
         d.next=e;
         e.next=f;
+        f.next=g;
         Node temp=a;
-        for(int i=0;i<6;i++){
-            System.out.print(temp.data + " ");
+//        for(int i=0;i<6;i++){
+//            System.out.print(temp.data + " ");
+//            temp=temp.next;
+//        }
+
+        while(temp!=null){
+            System.out.print(temp.data+" ");
             temp=temp.next;
         }
+
     }
 }
